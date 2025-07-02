b8f63c451c9887f36f6be3b1d17757cb67044b39
Melek Karataş <melek022233@gmail.com>
2025-07-02 12:42:16 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Lütfen 1. sayıyı giriniz");
+int sayi1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Lütfen 2. sayıyı giriniz");
+int sayi2 = int.Parse(Console.ReadLine());
+int orjsayi1 = sayi2, orjsayi2 = sayi1;
+
+sayi1 = (sayi1 < 0) ? -sayi1 : sayi1;
+sayi2 = (sayi2 < 0) ? -sayi2 : sayi2;
+while (sayi1 != sayi2)
+{
+    if (sayi1 > sayi2)
+    {
+        sayi1 -= sayi2;
+    }
+    else
+    {
+        sayi1 -= sayi2;
+    }
+}
+
+Console.WriteLine("EBOB {0},{1}= {2}", orjsayi1, orjsayi2, sayi1);
